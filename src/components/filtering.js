e64ce229585a9b7b7dd0c0bfb98f455fa319f809
xiwes <152086132+xiwes@users.noreply.github.com>
2025-10-25 08:50:34 +0300

    Update filtering.js

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,42 +1,51 @@
 export function initFiltering(elements) {
-    const updateIndexes = (elements, indexes) => {
-        Object.keys(indexes).forEach((elementName) => {
-            const selectEl = elements[elementName];
-            if (!selectEl) return;
-            // очистим старые
-            selectEl.replaceChildren();
-            Object.values(indexes[elementName]).forEach(name => {
-                const el = document.createElement('option');
-                el.textContent = name;
-                el.value = name;
-                selectEl.appendChild(el);
-            });
-        });
-    };
+  const updateIndexes = (elements, indexes) => {
+    Object.keys(indexes).forEach((elementName) => {
+      const selectEl = elements[elementName];
+      if (!selectEl) return;
 
-    const applyFiltering = (query, state, action) => {
-        // очистка отдельного поля по кнопке в UI (опционально)
-        if (action && action.name === 'clear' && action.dataset?.field) {
-            const fieldName = action.dataset.field;
-            const input = elements[fieldName];
-            if (input && 'value' in input) input.value = '';
-            if (fieldName in state) state[fieldName] = '';
-        }
+      const prev = selectEl.value;
 
-        const filter = {};
-        Object.keys(elements).forEach(key => {
-            const el = elements[key];
-            if (!el) return;
-            if (['INPUT', 'SELECT', 'TEXTAREA'].includes(el.tagName) && el.value) {
-                filter[`filter[${el.name}]`] = el.value;
-            }
-        });
+      while (selectEl.options.length > 1) {
+        selectEl.remove(1);
+      }
 
-        return Object.keys(filter).length ? Object.assign({}, query, filter) : query;
-    };
+      const opts = Object.values(indexes[elementName]).map((name) => {
+        const opt = document.createElement('option');
+        opt.value = name;
+        opt.textContent = name;
+        return opt;
+      });
+      selectEl.append(...opts);
 
-    return {
-        updateIndexes,
-        applyFiltering
-    };
+      if (prev && opts.some(o => o.value === prev)) {
+        selectEl.value = prev;
+      } else {
+        selectEl.value = '';
+      }
+    });
+  };
+
+  const applyFiltering = (query, state, action) => {
+    if (action?.name === 'clear' && action.dataset?.field) {
+      const field = action.dataset.field;
+      const el = Object.values(elements).find(el => el?.name === field);
+      if (el) el.value = '';
+    }
+
+    const filter = {};
+    Object.keys(elements).forEach((key) => {
+      const el = elements[key];
+      if (!el) return;
+      if (['INPUT', 'SELECT'].includes(el.tagName) && el.value) {
+        filter[`filter[${el.name}]`] = el.value;
+      }
+    });
+
+    return Object.keys(filter).length
+      ? Object.assign({}, query, filter)
+      : query;
+  };
+
+  return { updateIndexes, applyFiltering };
 }
